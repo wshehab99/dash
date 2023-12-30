066f7b1a40e6a5fefb20279a976d9cbe8a24433c
Waleed Bin Shehab <waleedbinshihab@gmail.com>
2023-12-30 12:28:21 +0300

    post resource

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,6 +48,6 @@ class User extends Authenticatable implements  FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return  $this->hasRole('admin');
+        return  $this->hasRole(['admin','writer','user']);
     }
 }
